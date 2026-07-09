a569ea8a4f907b28c6b158131450ba697c36242c
Wainaina Erastus <wainainaerastus2@gmail.com>
2026-07-09 16:02:23 +0300

    linked google fonts on the project

---
+++ src/mockData/mockData.js
@@ -0,0 +1,48 @@
+export const NavbarMenu = [
+  {
+    id: 1,
+    title: "Home",
+    link: "/",
+  },
+
+  {
+    id: 2,
+    title: "Why HomeNet",
+    link: "/about",
+  },
+
+  {
+    id: 3,
+    title: "Plans",
+    link: "/plans",
+  },
+
+  {
+    id: 4,
+    title: "Availability",
+    link: "/CoverageChecker",
+  },
+
+  {
+    id: 5,
+    title: "Speed check",
+    link: "/speedcheck",
+  },
+
+  {
+    id: 6,
+    title: "FAQS",
+    link: "FaqSection ",
+  },
+  {
+    id: 7,
+    title: "News",
+    link: "news",
+  },
+
+  {
+    id: 8,
+    title: "Contact",
+    link: "contact",
+  },
+];
